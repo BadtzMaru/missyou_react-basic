2fd46643446ab62d714043b99d77b47ce4d6972e
BadtzMaru <707380414@qq.com>
2020-10-28 01:14:53 +0800

    组件的初步使用

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
 	render() {
-		return React.createElement(
-			'div',
-			{ className: 'App' },
-			React.createElement('h1', null, 'hello world')
+		return (
+			<div className='App'>
+				<h1>Hello World</h1>
+				<p>Hi, React App</p>
+				<Person />
+				<Person />
+				<Person />
+				<Person />
+			</div>
 		);
 	}
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Person = () => {
+	return <p>大家好,我是陈炜嘉</p>;
+};
+
+export default Person;
